b0784d599885cb6737566f5c823b58cea3038a01
Iyamu Joshua <ayevbeosa.j@gmail.com>
2020-05-23 13:34:58 +0100

    Create fields.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:tradetrack/authentication.dart';
 
 class HomePage extends StatelessWidget {
+  final BaseAuth auth;
+  final VoidCallback logoutCallback;
+  final String userId;
+
+  const HomePage({Key key, this.auth, this.logoutCallback, this.userId})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
