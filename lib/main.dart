34f0f6bc9428ec90dcf514f9bf8d5c8250e3bdda
Iyamu Joshua <ayevbeosa.j@gmail.com>
2020-05-23 20:34:39 +0100

    Change App title. Clean up code. Remove unused code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-import './register_page.dart';
-import './sign_in_page.dart';
+import 'package:tradetrack/authentication.dart';
+import 'package:tradetrack/root_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,61 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Trade Track',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'TradeTrack'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  FirebaseUser user;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            child: RaisedButton(
-              child: const Text('Register'),
-              onPressed: () => _pushPage(context, RegisterPage()),
-            ),
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-          ),
-          Container(
-            child: RaisedButton(
-              child: const Text('SignIn'),
-              onPressed: () => _pushPage(context, SignInPage()),
-            ),
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-          ),
-        ],
+      home: RootPage(
+        auth: Auth(),
       ),
     );
   }
-
-  void _pushPage(BuildContext context, Widget page) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(builder: (_) => page),
-    );
-  }
 }
+
+//void _pushPage(BuildContext context, Widget page) {
+//  Navigator.of(context).push(
+//    MaterialPageRoute<void>(builder: (_) => page),
+//  );
+//}
