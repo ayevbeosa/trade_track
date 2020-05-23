ac3249f51e73ab53404e52c86b9d8b54f2257a38
Iyamu Joshua <ayevbeosa.j@gmail.com>
2020-05-23 11:06:57 +0100

    Add demo sign up page

---
+++ lib/login_signup_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginSignUpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Trade Track"),
+      ),
+      body: Container(
+        child: Text("Something Something"),
+      ),
+    );
+  }
+
+}
